ab065d0fcc4f5097b8a5a9f17f99a41ff0316e38
pedromcunha <pedro.spinsomewebs@gmail.com>
2015-01-10 14:35:56 -0500

    updated mission to only apply to trees

--- main.js
+++ main.js
@@ -2,7 +2,25 @@
 var ChallengeApp = ChallengeApp || {};
 
 ChallengeApp.moveSentence = function() {
-    console.log('oops not working yet, try again later');
+    //create a copy of the node
+    this.remove();
+
+    // var xmlhttp;
+    // //in the case of modern browser IE7+, FF, Chrome, opera, safari
+    // if(window.XMLHttpRequest) {
+    //   xmlhttp = new XMLHttpRequest();
+    // }
+    // //in the case of older browser IE6 + IE5
+    // else {
+    //   xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    // }    
+    // xmlhttp.onreadystatechange = function() {
+    //     if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+    //         document.getElementById("rightSide").getElementsByClassName('sentence-container') = xmlhttp.responseText;
+    //     }
+    // }
+    // xmlhttp.open("GET","ajax_info.txt",true);
+    // xmlhttp.send();
 };
 
 
@@ -16,3 +34,14 @@ ChallengeApp.init = function() {
 }
 
 ChallengeApp.init();
+
+
+// xmlhttp.onreadystatechange=function()
+//   {
+//   if (xmlhttp.readyState==4 && xmlhttp.status==200)
+//     {
+//     document.getElementById("myDiv").innerHTML=xmlhttp.responseText;
+//     }
+//   }
+// xmlhttp.open("GET","ajax_info.txt",true);
+// xmlhttp.send();
